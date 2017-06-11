97ff78e574dbb60c2b9bff1c7d45deccc21e4254
Ritzlgrmft <markus.wagner02@web.de>
2017-06-11 13:07:54 +0200

    fix: add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+export { AttachmentState } from "./attachment-state.model";
+export { FeedbackConfiguration } from "./feedback-configuration.model";
+export { FeedbackContact } from "./feedback-contact.model";
 export { FeedbackModule } from "./feedback.module";
 export { FeedbackService } from "./feedback.service";
-export { FeedbackViewerModalManager } from "./feedback-viewer-modal.manager";
 export { FeedbackViewerModalComponent } from "./feedback-viewer-modal.component";
+export { FeedbackViewerModalManager } from "./feedback-viewer-modal.manager";
 export { FeedbackViewerTranslation } from "./feedback-viewer-translation.model";
-export { AttachmentState } from "./attachment-state.model";
